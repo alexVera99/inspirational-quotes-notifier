305a389ee9cb3f4df443a358512900405d4961b3
alexVera99 <alexanderjoel.vera99@gmail.com>
2023-10-04 02:46:53 +0200

    test: add test to get one quote from api

--- src/apiReader.py
+++ src/apiReader.py
@@ -7,6 +7,8 @@ from logger import Logger
 from validator import DataValidator
 
 class ApiReader:
+    DEFAULT_QUOTE_TEXT = "Play wisely the cards life gave you."
+    DEFAULT_QUOTE_AUTHOR = "Yourself"
 
     def __init__(self, default_author='Unknown', api_url = "https://type.fit/api/quotes"):
         # Max size of the quote to be displayed correctly in the notifications
@@ -18,15 +20,13 @@ class ApiReader:
         # Initializing the status code
         self.status_code = 0
 
-        # Default Inspiration Quote when no Internet connection
-        self.quote_text = "Play wisely the cards life gave you."
-        self.quote_author = "Yourself"
-
         # Initializing the logger
         self.logger = Logger()
 
         # Data validator
         self.validator = DataValidator(default_author)
+
+        self.max_num_quotes_retries = 10
 
         # API url
         self.api_url = api_url
@@ -52,19 +52,24 @@ class ApiReader:
 
     def getOneQuote(self, debug = False):
         r = self.readQuotesFromAPI(debug)
-        if(self.status_code == 200):
-            # reading the JSON information
-            data = r.json()
-            data_size = len(data)
 
-            while(True):
-                # Generating a random number to choose a random quote
-                seed = random.randint(0, data_size-1)
+        if self.status_code != 200:
+            return self.DEFAULT_QUOTE_TEXT, self.DEFAULT_QUOTE_AUTHOR
 
-                # Choose the quote
-                quote = data[seed]
-                success, self.quote_text, self.quote_author = self.validator.validateData(quote["text"], quote["author"])
+        # reading the JSON information
+        data = r.json()
+        data_size = len(data)
 
-                if (success & len(self.quote_text) <= self.max_size_quote):
-                    break
-        return self.quote_text, self.quote_author
+        for _ in range(0, self.max_num_quotes_retries):
+            # Generating a random number to choose a random quote
+            seed = random.randint(0, data_size-1)
+
+            # Choose the quote
+            quote = data[seed]
+            _, quote_text, quote_author = self.validator.validateData(quote["text"], quote["author"])
+
+            if len(quote_text) <= self.max_size_quote:
+                return quote_text, quote_author
+
+        # TODO: A log saying that no quote was valid could be nice
+        return self.DEFAULT_QUOTE_TEXT, self.DEFAULT_QUOTE_AUTHOR
--- tests/test_api_reader.py
+++ tests/test_api_reader.py
@@ -50,3 +50,87 @@ def test_read_quotes_from_api_raise_request_exception(mock_requests_get: Mock):
     assert res is None
     assert mock_requests_get.call_count == api_reader.max_num_requests
     assert api_reader.status_code == -1
+
+
+@patch("requests.get")
+def test_get_one_quote_happy_path(mock_requests_get: Mock):
+    default_author = "Pepe"
+    api_url = "http://www.test.invalid"
+    api_reader = ApiReader(default_author, api_url)
+
+    status_code = 200
+    expected_text = "Some text"
+    expected_author = "Some author"
+    mock_response = Mock()
+    mock_response.status_code = status_code
+    mock_response.json.return_value = [
+        {
+            "text": expected_text,
+            "author": expected_author
+        }
+    ]
+
+    mock_requests_get.return_value = mock_response
+    q_text, q_author = api_reader.getOneQuote()
+
+    assert expected_text == q_text
+    assert expected_author == q_author
+
+
+@patch("requests.get")
+def test_get_one_quote_cannot_generate_proper_quote(mock_requests_get: Mock):
+    default_author = "Pepe"
+    api_url = "http://www.test.invalid"
+    api_reader = ApiReader(default_author, api_url)
+
+    status_code = 200
+    mock_response = Mock()
+    mock_response.status_code = status_code
+    mock_response.json.return_value = [
+        {
+            "text": None,
+            "author": "Some author"
+        }
+    ]
+
+    mock_requests_get.return_value = mock_response
+    q_text, q_author = api_reader.getOneQuote()
+
+    assert "" == q_text
+    assert "" == q_author
+
+
+@patch("requests.get")
+def test_get_one_quote_too_large(mock_requests_get: Mock):
+    default_author = "Pepe"
+    api_url = "http://www.test.invalid"
+    api_reader = ApiReader(default_author, api_url)
+
+    status_code = 200
+    too_large_quote_text = "a" * (api_reader.max_size_quote + 1)
+    mock_response = Mock()
+    mock_response.status_code = status_code
+    mock_response.json.return_value = [
+        {
+            "text": too_large_quote_text,
+            "author": "Some author"
+        }
+    ]
+
+    mock_requests_get.return_value = mock_response
+    q_text, q_author = api_reader.getOneQuote()
+
+    assert ApiReader.DEFAULT_QUOTE_TEXT == q_text
+    assert ApiReader.DEFAULT_QUOTE_AUTHOR == q_author
+
+
+@patch("requests.get", side_effect=RequestException)
+def test_get_one_quote_no_internet_connection(mock_requests_get: Mock):
+    default_author = "Pepe"
+    api_url = "http://www.test.invalid"
+    api_reader = ApiReader(default_author, api_url)
+
+    q_text, q_author = api_reader.getOneQuote()
+
+    assert ApiReader.DEFAULT_QUOTE_TEXT == q_text
+    assert ApiReader.DEFAULT_QUOTE_AUTHOR == q_author
