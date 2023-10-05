00dc7de51efe99b64ffd7040d0607af7ad2bf3e7
alexVera99 <alexanderjoel.vera99@gmail.com>
2023-10-05 12:18:31 +0200

    fix: remove imports of NotifierOSx to make it pass in CI

--- tests/notifications/macos/test_notifier_osx.py
+++ tests/notifications/macos/test_notifier_osx.py
@@ -1,7 +1,6 @@
 from unittest.mock import ANY
 from unittest.mock import Mock
 
-from inspi_quote_notifier.notifications.macos.notifier_osx import NotifierOSX
 from inspi_quote_notifier.quotes.domain.quote import Quote
 
 
@@ -18,5 +17,5 @@ def test_notify(notifier_osx: Mock, mock_pync_notifier):
         quote.text,
         title=expected_notification_title,
         group=ANY,
-        sound=NotifierOSX.sound,
+        sound=notifier_osx.sound,
     )
