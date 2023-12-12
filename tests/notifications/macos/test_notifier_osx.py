7994fdaa0a195b0da018e7348cd8f4464fe77fac
Alexander Vera <alexanderjoel.vera99@gmail.com>
2023-12-13 00:58:49 +0100

    fixup! fix: mock the Notify from MacOs to make it work in ci

--- tests/notifications/macos/test_notifier_osx.py
+++ tests/notifications/macos/test_notifier_osx.py
@@ -1,7 +1,27 @@
+import sys
 from unittest.mock import ANY
+from unittest.mock import MagicMock
 from unittest.mock import Mock
+from unittest.mock import patch
 
+import pytest
 from inspi_quote_notifier.quotes.domain.quote import Quote
+
+
+sys.modules["pync"] = MagicMock()
+
+
+@pytest.fixture
+def mock_pync_notify():
+    with patch("pync.notify") as mock_notify:
+        yield mock_notify
+
+
+@pytest.fixture
+def notifier_osx(mock_pync_notify):
+    from inspi_quote_notifier.notifications.macos.notifier_osx import NotifierOSX
+
+    yield NotifierOSX()
 
 
 def test_notify(notifier_osx: Mock, mock_pync_notify):
